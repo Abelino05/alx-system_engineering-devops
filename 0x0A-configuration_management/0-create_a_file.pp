51371a1de4c6b8eafbbe4b4f4dd42446e98aa466
Abelino05 <pakpaliabel@gmail.com>
2023-01-12 17:35:33 +0000

    Puppet tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creation of a file in the /tmp directory
+file { '/tmp/holberton':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+Tasks on configuration management with puppet.
